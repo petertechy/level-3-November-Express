fec5a5cc52e6f193f2d6ca511a4758c9b33b0c55
Petertechy <petertechy01@gmail.com>
2025-12-04 22:17:49 +0100

    Update utils

---
+++ utils/endpoint.js
@@ -0,0 +1,20 @@
+export const endpoint = [
+  {
+    firstname: "Adeniyi",
+    lastname: "Opeyemi",
+    age: 20,
+    dob: "20-03-1998",
+  },
+  {
+    firstname: "Afolabi",
+    lastname: "Rachael",
+    age: 22,
+    dob: "19-02-1994",
+  },
+  {
+    firstname: "Ayelabawo",
+    lastname: "Titilayo",
+    age: 15,
+    dob: "15-05-2003",
+  },
+];
